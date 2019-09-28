e01f572cf2e475813f3dbc4a3bdca8100dea4d02
RadoslawSzymansky <radek199@gmail.com>
2019-09-28 11:55:30 +0200

    change base url in cofig for env

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env === 'production' ? '' : 'http://localhost:5000';
+export const BASE_URL = '';
--- server/server.js
+++ server/server.js
@@ -29,9 +29,7 @@ app.use('/api/users', userRoutes);
 
 
 // Serve static files from the React app if production env
-if (process.env === 'production') {
-  app.use(express.static(path.join(__dirname, '/../client/build/public/')));
-}
+app.use(express.static(path.join(__dirname, '/../client/build/public/')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/build/public/index.html'));
