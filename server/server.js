a2afc6c468e750d4954b84b9fb91af20d7a90fb8
RadoslawSzymansky <radek199@gmail.com>
2019-09-28 11:19:24 +0200

    addd dot to script path

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use('/api/users', userRoutes);
 
 // Serve static files from the React app if production env
 if (process.env === 'PRODUCTION') {
-  app.use(express.static(path.join(__dirname, '/../client/build/public')));
+  app.use(express.static(path.join(__dirname, '/../client/build/public/')));
 }
 
 app.get('*', (req, res) => {
